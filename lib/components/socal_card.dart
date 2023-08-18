9f5b870f181709a85eff1f66801574e06d7e0bbd
Mujtaba <gmjatoi789@gmail.com>
2023-08-19 00:06:31 +0500

    Complete application structure

---
+++ lib/components/socal_card.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../config/size_config.dart';
+
+class SocalCard extends StatelessWidget {
+  const SocalCard({
+    Key? key,
+    this.icon,
+    this.press,
+  }) : super(key: key);
+
+  final String? icon;
+  final Function? press;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press as void Function()?,
+      child: Container(
+        margin: EdgeInsets.symmetric(
+            horizontal: 10), //getProportionateScreenWidth(10)),
+        padding: EdgeInsets.all(12), //getProportionateScreenWidth(12)),
+        height: 40, //getProportionateScreenHeight(40),
+        width: 40, //getProportionateScreenWidth(40),
+        decoration: BoxDecoration(
+          color: Color(0xFFF5F6F9),
+          shape: BoxShape.circle,
+        ),
+        child: SvgPicture.asset(icon!),
+      ),
+    );
+  }
+}
---
+++ lib/ui/pages/sign_in/components/sign_form.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:testapp/components/custom_surfix_icon.dart';
+// import 'package:testapp/components/form_error.dart';
+// import 'package:testapp/helper/keyboard.dart';
+// import 'package:testapp/ui/pages/forgot_password/forgot_password_screen.dart';
+// import 'package:testapp/ui/pages/login_success/login_success_screen.dart';
+
+import '../../../../components/default_button.dart';
+import '../../../../config/constants.dart';
+import '../../../../config/size_config.dart';
+
+class SignForm extends StatefulWidget {
+  @override
+  _SignFormState createState() => _SignFormState();
+}
+
+class _SignFormState extends State<SignForm> {
+  final _formKey = GlobalKey<FormState>();
+  String? email;
+  String? password;
+  bool? remember = false;
+  final List<String?> errors = [];
+
+  void addError({String? error}) {
+    if (!errors.contains(error))
+      setState(() {
+        errors.add(error);
+      });
+  }
+
+  void removeError({String? error}) {
+    if (errors.contains(error))
+      setState(() {
+        errors.remove(error);
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          buildEmailFormField(),
+          SizedBox(height: 30),
+          buildPasswordFormField(),
+          SizedBox(height: 30),
+          Row(
+            children: [
+              Checkbox(
+                value: remember,
+                activeColor: kPrimaryColor,
+                onChanged: (value) {
+                  setState(() {
+                    remember = value;
+                  });
+                },
+              ),
+              Text("Remember me"),
+              Spacer(),
+              GestureDetector(
+                onTap: () => print(""),
+              )
+            ],
+          ),
+          // FormError(errors: errors),
+          SizedBox(height: 20),
+          DefaultButton(
+            text: "Continue",
+            press: () {
+              if (_formKey.currentState!.validate()) {
+                _formKey.currentState!.save();
+                // if all are valid then go to success screen
+                // KeyboardUtil.hideKeyboard(context);
+                // Navigator.pushNamed(context, LoginSuccessScreen.routeName);
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+      obscureText: true,
+      onSaved: (newValue) => password = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: kPassNullError);
+        } else if (value.length >= 8) {
+          removeError(error: kShortPassError);
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          addError(error: kPassNullError);
+          return "";
+        } else if (value.length < 8) {
+          addError(error: kShortPassError);
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: "Password",
+        hintText: "Enter your password",
+        // If  you are using latest version of flutter then lable text and hint text shown like this
+        // if you r using flutter less then 1.20.* then maybe this is not working properly
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Lock.svg"),
+      ),
+    );
+  }
+
+  TextFormField buildEmailFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (newValue) => email = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: kEmailNullError);
+        } else if (emailValidatorRegExp.hasMatch(value)) {
+          removeError(error: kInvalidEmailError);
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          addError(error: kEmailNullError);
+          return "";
+        } else if (!emailValidatorRegExp.hasMatch(value)) {
+          addError(error: kInvalidEmailError);
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: "Email",
+        hintText: "Enter your email",
+        // If  you are using latest version of flutter then lable text and hint text shown like this
+        // if you r using flutter less then 1.20.* then maybe this is not working properly
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Mail.svg"),
+      ),
+    );
+  }
+}
