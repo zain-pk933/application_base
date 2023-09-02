49215d3b71337c2f33f0eb108f392e541025db43
Mujtaba <gmjatoi789@gmail.com>
2023-09-02 10:10:55 +0500

    home screen assets and changes in sign in and helper files

--- lib/ui/pages/sign_in/components/sign_form.dart
+++ lib/ui/pages/sign_in/components/sign_form.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:testapp/components/custom_surfix_icon.dart';
-// import 'package:testapp/components/form_error.dart';
-// import 'package:testapp/helper/keyboard.dart';
-// import 'package:testapp/ui/pages/forgot_password/forgot_password_screen.dart';
-// import 'package:testapp/ui/pages/login_success/login_success_screen.dart';
+import 'package:testapp/components/form_error.dart';
+import 'package:testapp/helper/keyboard.dart';
+import 'package:testapp/ui/pages/forgot_password/forgot_password_screen.dart';
+import 'package:testapp/ui/pages/login_success/login_success_screen.dart';
 
 import '../../../../components/default_button.dart';
-import '../../../../config/constants.dart';
-import '../../../../config/size_config.dart';
+import '../../../../constants.dart';
+import '../../../../size_config.dart';
 
 class SignForm extends StatefulWidget {
   @override
@@ -42,9 +42,9 @@ class _SignFormState extends State<SignForm> {
       child: Column(
         children: [
           buildEmailFormField(),
-          SizedBox(height: 30),
+          SizedBox(height: getProportionateScreenHeight(30)),
           buildPasswordFormField(),
-          SizedBox(height: 30),
+          SizedBox(height: getProportionateScreenHeight(30)),
           Row(
             children: [
               Checkbox(
@@ -59,20 +59,27 @@ class _SignFormState extends State<SignForm> {
               Text("Remember me"),
               Spacer(),
               GestureDetector(
-                onTap: () => print(""),
+                onTap: () => Navigator.pushNamed(
+                    context, ForgotPasswordScreen.routeName),
+                child: Text(
+                  "Forgot Password",
+                  style: TextStyle(decoration: TextDecoration.underline),
+                ),
               )
             ],
           ),
-          // FormError(errors: errors),
-          SizedBox(height: 20),
+          FormError(errors: errors),
+          SizedBox(height: getProportionateScreenHeight(20)),
           DefaultButton(
             text: "Continue",
             press: () {
               if (_formKey.currentState!.validate()) {
                 _formKey.currentState!.save();
                 // if all are valid then go to success screen
-                // KeyboardUtil.hideKeyboard(context);
+                KeyboardUtil.hideKeyboard(context);
                 // Navigator.pushNamed(context, LoginSuccessScreen.routeName);
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => LoginSuccessScreen()));
               }
             },
           ),
